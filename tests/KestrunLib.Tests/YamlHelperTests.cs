bf2025ebd2f7afab3820b61cce923264d07a6818
mdaneri <17148649+mdaneri@users.noreply.github.com>
2025-07-17 17:36:59 -0700

    Add additional unit tests and CI

---
+++ tests/KestrunLib.Tests/YamlHelperTests.cs
@@ -0,0 +1,34 @@
+using KestrumLib;
+using System.Collections;
+using System.Management.Automation;
+using Xunit;
+
+public class YamlHelperTests
+{
+    [Fact]
+    public void ToYaml_SerializesObject()
+    {
+        var ht = new Hashtable { { "name", "foo" }, { "value", 1 } };
+        var yaml = YamlHelper.ToYaml(ht);
+        Assert.Contains("name: foo", yaml);
+        Assert.Contains("value: 1", yaml);
+    }
+
+    [Fact]
+    public void FromYamlToHashtable_RoundTrip()
+    {
+        string yaml = "name: foo\nvalue: 1";
+        var ht = YamlHelper.FromYamlToHashtable(yaml);
+        Assert.Equal("foo", ht["name"]);
+        Assert.Equal(1, (int)(ht["value"]));
+    }
+
+    [Fact]
+    public void FromYamlToPSCustomObject_RoundTrip()
+    {
+        string yaml = "name: foo\nvalue: 1";
+        var obj = YamlHelper.FromYamlToPSCustomObject(yaml);
+        Assert.Equal("foo", obj.Members["name"].Value);
+        Assert.Equal(1, (int)obj.Members["value"].Value);
+    }
+}
